5c56dfb199adfd225200822d8594d42c7cf0f5aa
kwijik <joebalalaika@ya.ru>
2018-07-20 10:59:02 +0200

    add file cleaner

---
+++ Cleaner.py
@@ -0,0 +1,22 @@
+from pandas import DataFrame
+from pandas import read_csv
+
+df = read_csv("Moyennes_J_creil_2005_2015.csv", header=None, sep=';', decimal=',')
+df = df.iloc[1:,]
+#print(df.head())
+date = df.iloc[:,0]
+
+df = df.drop([0], axis=1)
+df = df.astype(float)
+#print(df.head())
+df[0] = date
+df.dropna()
+print(df.head())
+
+df.sort_index(axis=1, inplace=True)
+
+print()
+print(df.head())
+
+# transform in floats
+df.to_csv("df_creil.csv")
